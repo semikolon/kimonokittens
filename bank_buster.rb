322e27570cdd23d30918ae97bc44bcade31ef933
Fredrik Bränström <fredrik@branstrom.name>
2023-08-18 17:01:00 +0200

    Error & timeout handling in BankBuster. Add QR_CODE_PICKED_UP

--- bank_buster.rb
+++ bank_buster.rb
@@ -37,18 +37,18 @@ class BankBuster < Vessel::Cargo
   INFO_INTERVAL = 20
   MAX_ATTEMPTS = 10 # Define the maximum number of attempts
   driver :ferrum,
-  # slowmo: 2.0,
-  # port: 9222, # for remote debugging
-  # host: 'localhost',
-  save_path: TRANSACTIONS_DIR,
-  # headless: false,
-  browser_options: {
-      'no-default-browser-check' => true,
-      'disable-extensions' => true,
-      'disable-translate' => true,
-      'mute-audio' => true,
-      'disable-sync' => true
-      #"proxy-bypass-list" => "<-loopback>"
+    # slowmo: 2.0,
+    # port: 9222, # for remote debugging
+    # host: 'localhost',
+    save_path: TRANSACTIONS_DIR,
+    # headless: false,
+    browser_options: {
+        'no-default-browser-check' => true,
+        'disable-extensions' => true,
+        'disable-translate' => true,
+        'mute-audio' => true,
+        'disable-sync' => true
+        #"proxy-bypass-list" => "<-loopback>"
     },
     # proxy: { host: "localhost", port: "3399" },
     # logger: FerrumLogger.new(Logger.new("browser.log")),
@@ -58,42 +58,42 @@ class BankBuster < Vessel::Cargo
     domain ENV['BANK_DOMAIN']
     start_urls ENV['BANK_LOGIN_URL']
     # TODO: Start on /start-page and reuse login from last session if possible, need to save and reuse cookies for that though
-    
-    def parse(&block)
-      # Filter out non-essential requests
-      filter_out_non_essentials
-      wait_for_idle_or_rescue
-      # Accept cookies on the page
-      accept_cookies
-      page.network.wait_for_idle(timeout: 1)
-      # Start the login process
-      login_process
-      # Retrieve files and pass the block to the method
-      retrieve_files(&block)
-      # Reset the browser to be able to reuse the crawler instance
-      reset_browser
+  
+  def parse(&block)
+    # Filter out non-essential requests
+    filter_out_non_essentials
+    wait_for_idle_or_rescue
+    # Accept cookies on the page
+    accept_cookies
+    wait_for_idle_or_rescue(timeout: 1)
+    # Start the login process
+    login_process
+    # Retrieve files and pass the block to the method
+    retrieve_files(&block)
+    # Reset the browser to be able to reuse the crawler instance
+    reset_browser
 
-    rescue LoginError => e
-      handle_login_errors(e)
-    rescue FileRetrievalError => e
-      handle_file_errors(e)
-    rescue Ferrum::DeadBrowserError => e
-      puts "Browser died.".red
-    rescue NoMethodError => e
-      if e.backtrace[0].include?('ferrum') && e.message.include?('command')
-        puts "Tried to command dead browser.".orange
-      else
-        raise # Unexpected error
-      end
+  rescue LoginError => e
+    handle_login_errors(e)
+  rescue FileRetrievalError => e
+    handle_file_errors(e)
+  rescue Ferrum::DeadBrowserError => e
+    puts "Browser died.".red
+  rescue NoMethodError => e
+    if e.backtrace[0].include?('ferrum') && e.message.include?('command')
+      puts "Tried to command dead browser.".orange
+    else
+      raise # Unexpected error
     end
+  end
 
-    def wait_for_idle_or_rescue(...)
-      wait_for_idle(...)
-    rescue Ferrum::TimeoutError
-      yield({ type: 'ERROR', error: 'Timeout while waiting for update' })
-    end
+  def wait_for_idle_or_rescue(...)
+    wait_for_idle(...)
+  rescue Ferrum::TimeoutError
+    yield({ type: 'ERROR', error: 'Timeout while waiting for update' })
+  end
 
-    def filter_out_non_essentials
+  def filter_out_non_essentials
     # Intercept network requests
     page.network.intercept
     page.on(:request) do |request|
@@ -115,20 +115,20 @@ class BankBuster < Vessel::Cargo
     puts 'Trying to identify cookie accept button...'.gray
     attempts = 0
     until cookie_button = at_css("acorn-button[data-cy='acorn-button-accept-all-cookies']")
-      raise 'Could not find cookie accept button' if attempts > 100
+      raise LoginError, 'Could not find cookie accept button' if attempts > 100
       sleep 0.1
       attempts += 1
     end
     cookie_button.click
   end
 
-    def input_login_and_get_qr_code
-      ssn_field = at_css("input[type=text]")
-      raise LoginError, 'SSN field not found' unless ssn_field
-      ssn_field.focus.type(SSN, :enter)
-      puts 'Filled in SSN, starting login...'
-    
-      wait_for_idle_or_rescue
+  def input_login_and_get_qr_code
+    ssn_field = at_css("input[type=text]")
+    raise LoginError, 'SSN field not found' unless ssn_field
+    ssn_field.focus.type(SSN, :enter)
+    puts 'Filled in SSN, starting login...'
+  
+    wait_for_idle_or_rescue
     message = at_css("span[slot=message]")&.text
     puts message if message
     raise LoginError, 'Unable to login' if message&.include?('Det gick inte att logga in')
@@ -140,7 +140,7 @@ class BankBuster < Vessel::Cargo
       page.screenshot(path: qr_code_url, selector: 'img.mobile-bank-id__qr-code--image')
       yield({ type: 'QR_UPDATE', qr_code_url: qr_code_url })
       sleep 1
-      page.network.wait_for_idle(timeout: 1)
+      wait_for_idle_or_rescue(timeout: 1)
     end
     puts "\n"
 
@@ -195,7 +195,7 @@ class BankBuster < Vessel::Cargo
     url_to_fetch_url = absolute_url('/TDE_DAP_Portal_REST_WEB/api/v5/message/document/'+id+'/view?referenceType=DOCUMENT_ID')
     page.go_to(url_to_fetch_url)
     
-    page.network.wait_for_idle(timeout: 1) unless page.network.status == 200
+    wait_for_idle_or_rescue(timeout: 1) unless page.network.status == 200
     raise FileRetrievalError, 'Download URL fetch failed' unless page.network.status == 200
     
     location_info = Oj.safe_load(page.at_css('pre').text)
@@ -205,8 +205,8 @@ class BankBuster < Vessel::Cargo
     url_to_download = absolute_url('/'+location_info['url']+'?ticket='+ticket)
     page.go_to(url_to_download)
     
-    page.network.wait_for_idle(timeout: 1) unless page.network.status == 200
-    raise 'Download failed' unless page.network.status == 200
+    wait_for_idle_or_rescue(timeout: 1) unless page.network.status == 200
+    raise FileRetrievalError, 'Download failed' unless page.network.status == 200
     
     filename = page.network.response.headers['Content-type'].match(/name="(.*)"/)[1]
     puts "Downloaded #{filename}".green
@@ -224,9 +224,7 @@ class BankBuster < Vessel::Cargo
       end
     end
     puts 'QR code picked up. Authenticate with BankID.'.yellow
-    # Add a rescue block to handle timeout errors
-    rescue Timeout::Error
-      yield({ type: 'ERROR', error: 'Timeout while waiting for update' })
+    yield({ type: 'QR_CODE_PICKED_UP' })
     
     until at_css('acorn-section-header')&.attribute('heading') == ENV['PROFILE_SELECT_TEXT']
       sleep 0.2
@@ -251,6 +249,12 @@ class BankBuster < Vessel::Cargo
   end
   
 
+  def retrieve_files
+    puts 'Logged in. Reading files...'.green
+    filenames = download_all_payment_files
+    yield({ type: 'FILES_RETRIEVED', filenames: filenames }) if block_given?
+  end
+
   def handle_errors(e, error_message, screenshot_path, &block)
     puts "\n#{error_message}".red
     page.screenshot(path: "#{SCREENSHOTS_DIR}/#{screenshot_path}")
@@ -273,12 +277,6 @@ class BankBuster < Vessel::Cargo
   
   def handle_file_errors(e)
     handle_errors(e, "Error while getting files.", 'error_files.jpg')
-  end
-
-  def retrieve_files
-    puts 'Logged in. Reading files...'.green
-    filenames = download_all_payment_files
-    yield({ type: 'FILES_RETRIEVED', filenames: filenames }) if block_given?
   end
 
   def reset_browser
