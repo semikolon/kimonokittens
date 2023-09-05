a1569a465a85ab0136dbba4409a6994b8a6340ef
Fredrik Bränström <fredrik@branstrom.name>
2023-09-05 21:10:09 +0200

    Refactor handler initialization and add socket connection logging
    - Refactored the handler's initialization to comment out the instance creation of `BankBuster`.
    - Added logging statements for successful socket connection opening and closing.
    - Modified the handling of messages to print out received messages and handle the start message differently.

--- handlers/bank_buster_handler.rb
+++ handlers/bank_buster_handler.rb
@@ -1,33 +1,39 @@
 require 'oj'
+require_relative '../bank_buster'
+require 'oj'
 
 class BankBusterHandler
   def initialize
-    @bank_buster = BankBuster.new
+    puts "Initializing..."
+    # comment out the instance 
+    #@bank_buster = BankBuster.new
   end
 
   def call(req)
     return unless req.upgrade?
 
     req.websocket do |ws|
+      ws.on_open do
+        puts "Socket connection opened successfully."
+      end
       ws.on_message do |msg|
         handle_message(ws, msg)
+      end
+      ws.on_close do 
+        puts "Closing socket connection."
       end
     end
   end
 
-def handle_message(ws, msg)
-  puts "Received: #{msg}"
-
-  case msg
-  when 'START'
-    begin
-      @bank_buster.parse do |result|
-        # Convert the result to JSON using Oj and send it to the frontend.
-        ws.write(Oj.dump(result))
-      end
-    rescue => e
-      # Send an error message to the frontend when an exception is raised.
-      ws.write("ERROR: #{e.message}")
+  def handle_message(ws, msg)
+    puts "Received message: #{msg}"
+    
+    # If msg == 'START', just print out we got the start message, but don't do any parsing
+    if msg == 'START'
+      puts "Received start message"
     end
+  # Comment the rescue part, let the exceptions propagate to get the default debugging behavior. 
+  # rescue => e
+  #   ws.write("ERROR: #{e.message}")
   end
 end
