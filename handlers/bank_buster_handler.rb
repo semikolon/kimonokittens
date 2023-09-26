1059ce68fcd9c25a54af3a22dbb5b97375c8c6cc
Fredrik Bränström <branstrom@gmail.com>
2023-09-26 16:01:33 +0200

    Update Gemfile.lock and bank_buster_handler.rb
    Gemfile.lock:
    - Updated agoo gem from version 2.15.5 to 2.15.7
    - Updated awesome_print gem from version 1.9.2 to 1.1.0
    - Added base64 gem with version 0.1.1
    - Updated colorize gem from version 0.8.1 to 1.1.0
    - Updated faraday gem from version 2.7.5 to 2.7.11
    - Updated ferrum gem from version 0.13 to 0.14
    - Updated mime-types gem from version 3.5.0 to 3..5..1
    - Updated oj gem from version 3..14..3 to 3..16..1
    - Updated ox gem from version 2..14..14 to .17
    
    bank_buster_handler.rb:
    - Removed unnecessary require_relative statements for bank_buster and Oj gems.
    - Added require statements for pry and agoo gems.
    - Modified call method parameters, replacing req with env.
    - Replaced req.upgrade? check with env['rack.upgrade?'] == :websocket check.
    - Modified ws.on_open, ws.on_message, and ws.on_close methods parameters, replacing req with upgraded.
    - Removed commented out code related to instance variable @bank_buster.

--- handlers/bank_buster_handler.rb
+++ handlers/bank_buster_handler.rb
@@ -1,39 +1,41 @@
+# require_relative '../bank_buster'
 require 'oj'
-require_relative '../bank_buster'
-require 'oj'
+require 'pry'
+require 'agoo'
 
 class BankBusterHandler
   def initialize
     puts "Initializing..."
-    # comment out the instance 
-    #@bank_buster = BankBuster.new
+    # @bank_buster = BankBuster.new
   end
 
-  def call(req)
-    return unless req.upgrade?
+  def call(env)
+    return unless env['rack.upgrade?'] == :websocket
 
-    req.websocket do |ws|
-      ws.on_open do
-        puts "Socket connection opened successfully."
-      end
-      ws.on_message do |msg|
-        handle_message(ws, msg)
-      end
-      ws.on_close do 
-        puts "Closing socket connection."
-      end
+    req = env['rack.hijack']
+    ws = Agoo::Upgraded.new(req, self)
+    ws.each do |msg|
+      handle_message(ws, msg)
     end
+  end
+
+  def on_open(upgraded)
+    puts "Socket connection opened successfully."
+  end
+
+  def on_message(upgraded, msg)
+    handle_message(upgraded, msg)
+  end
+
+  def on_close(upgraded)
+    puts "Closing socket connection."
   end
 
   def handle_message(ws, msg)
     puts "Received message: #{msg}"
     
-    # If msg == 'START', just print out we got the start message, but don't do any parsing
     if msg == 'START'
       puts "Received start message"
     end
-  # Comment the rescue part, let the exceptions propagate to get the default debugging behavior. 
-  # rescue => e
-  #   ws.write("ERROR: #{e.message}")
   end
 end
