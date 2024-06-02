6c444ce52da75deb23320a7ba3045d3bbbae9895
Fredrik Bränström <fredrik@branstrom.name>
2024-06-02 20:24:12 +0200

    Update schedule.rb for Raspberry Pi cron jobs
    Adjusted timing and added Tibber prices scraping to run every 2 hours.

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,7 @@
 
 
 # This is working on the Raspberry Pi
-# 0 0,6,12,18 * * * /bin/bash -l -c 'export PATH=~/.rbenv/shims:/usr/bin:$PATH; eval "$(rbenv init -)"; cd /home/pi/kimonokittens && bundle exec ruby /home/pi/kimonokittens/vattenfall.rb >> /home/pi/kimonokittens/cron_log.log 2>&1'
+# 0 0,6,12,18 * * * /bin/bash -l -c 'export PATH=~/.rbenv/shims:/usr/bin:$PATH; eval "$(rbenv init -)"; cd /home/pi/kimonokittens && timeout 3m bundle exec ruby /home/pi/kimonokittens/vattenfall.rb >> /home/pi/kimonokittens/cron_log.log 2>&1'
 
 job_type :rbenv_exec, %Q{export PATH=~/.rbenv/shims:/usr/bin:$PATH; eval "$(rbenv init -)"; \
                          cd :path && timeout 3m bundle exec :task :output }
@@ -15,11 +15,15 @@ set :output, "#{Dir.pwd}/cron_log.log"
 
 set :chronic_options, hours24: true
 
-every 4.hours do
+every 2.hours do
     rbenv_exec "ruby #{Dir.pwd}/vattenfall.rb"
 end
 
-# every 1.day, at: '17.15' do
-#     rbenv_exec "ruby #{Dir.pwd}/vattenfall.rb"
-# end
+every 2.hours do
+    rbenv_exec "ruby #{Dir.pwd}/tibber.rb"
+end
+
+#every 1.day, at: '16.22' do
+#    rbenv_exec "ruby #{Dir.pwd}/vattenfall.rb"
+#end
 
