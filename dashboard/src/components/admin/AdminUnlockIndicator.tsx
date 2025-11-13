e3704e08e77d31a4fe06444cd940a61cf268bfd6
Fredrik Bränström <branstrom@gmail.com>
2025-11-13 13:29:01 +0100

    Move admin unlock countdown to lower right

--- dashboard/src/components/admin/AdminUnlockIndicator.tsx
+++ dashboard/src/components/admin/AdminUnlockIndicator.tsx
@@ -37,7 +37,7 @@ export const AdminUnlockIndicator: React.FC = () => {
   const strokeDashoffset = circumference * (1 - progress)
 
   return (
-    <div className="fixed bottom-6 left-6 z-40">
+    <div className="fixed bottom-6 right-6 z-40">
       <div className="relative group">
         <svg className="w-12 h-12 -rotate-90" viewBox="0 0 40 40">
           <circle
